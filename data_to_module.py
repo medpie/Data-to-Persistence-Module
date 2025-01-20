f979c9c00ab3a7dbb600b18acff33a25ab91bf66
Mehdi N <127675274+medpie@users.noreply.github.com>
2025-01-19 20:05:15 -0600

    data_to_module.py

--- data_to_module.py
+++ data_to_module.py
@@ -3,19 +3,58 @@ import numpy as np
 from sklearn.metrics import pairwise_distances
 print("#####################################################################")
 
+""" List of functions:
+    - homology_generators(data, p)
+    - biRips_a(data, a, p)
+    - biRips(data, p)
+    - vertical_homology_mapping(data, a, p, r)
+    - horizontal_homology_mapping(data, a, p, r)
+    - data_to_pModule(data, p)
+"""
+
 def homology_generators(data, r):
      generators = []
-     
-     def loop_checker(array):
-        x, y = zip(*array)
-        list_of_endpoints = list(x) + list(y)
-        set_of_vertices = set(list_of_endpoints)
-        array_sum = sum(list_of_endpoints)
-        set_sum = sum(set(list_of_endpoints))
-        if array_sum == 2*set_sum:
-             return True
-        else:
-             False
+     def loop_finder(edges):
+      from collections import defaultdict
+
+      graph = defaultdict(list)
+      for a, b in edges:
+          graph[a].append(b)
+          graph[b].append(a)
+
+      visited = set()
+      loops = []
+
+      def dfs(vertex, parent, path):
+          visited.add(vertex)
+          path.append(vertex)
+
+          for neighbor in graph[vertex]:
+              if neighbor == parent:  
+                  continue
+              if neighbor in path:  
+                  loop_start_index = path.index(neighbor)
+                  loop_edges = [(path[i], path[i + 1]) for i in range(loop_start_index, len(path) - 1)]
+                  loop_edges.append((path[-1], neighbor))  
+                  loops.append(loop_edges)
+              elif neighbor not in visited:
+                  dfs(neighbor, vertex, path)
+
+          path.pop()  
+
+      for vertex in graph:
+          if vertex not in visited:
+              dfs(vertex, None, [])
+
+      unique_loops = []
+      seen_loops = set()
+      for loop in loops:
+          loop_tuple = tuple(sorted(loop))
+          if loop_tuple not in seen_loops and len(loop) > 2:
+              seen_loops.add(loop_tuple)
+              unique_loops.append(loop)
+
+      return unique_loops
 
      rips = gudhi.RipsComplex(points=data, max_edge_length=r)
      simplex_tree = rips.create_simplex_tree(max_dimension=2)
@@ -31,12 +70,10 @@ def homology_generators(data, r):
                     simplices_par.append(pair[0])
                     simplices_dic[parameter] = simplices_par
      for parameter in filtration:
-          if loop_checker(simplices_dic[parameter]) == True:
-                   list_of_simplices = []
-                   for simplex in simplices_dic[parameter]:
-                       list_of_simplices.append([data[simplex[0]], data[simplex[1]]])
-                   simplices_dic[parameter] = list_of_simplices
-                   generators.append(simplices_dic[parameter])
+          loops = loop_finder(simplices_dic[parameter])
+          if len(loops) != 0:
+             for loop in loops:
+                   generators.append([[data[simplex[0]], data[simplex[1]]] for simplex in loop])
 
      return generators
 
@@ -53,7 +90,7 @@ def biRips_a(data, a, p):
     observed_datapoints = set()
     gamma_function = density_function(data,p)
     for v in gamma_function.values():
-        if a <= v:
+        if v <= a + 1:
           for data_point in gamma_function.keys():
               if gamma_function[data_point] == v and data_point not in observed_datapoints:
                 observed_datapoints.add(data_point)
@@ -73,7 +110,10 @@ def biRips(data, p):
     if len(Rips) > 0:  
       for r in range(r_max):
         index_a_r = homology_generators(Rips, r)
-        biRips_dictionary.update({(a, r): index_a_r}) 
+        biRips_dictionary.update({(a, r): index_a_r})
+    else:
+        for r in range(r_max):
+           biRips_dictionary.update({(a, r): []})
 
   return biRips_dictionary
 
@@ -143,6 +183,61 @@ def data_to_pModule(data, p):
 
     return dictionary
 
-data = [[1, 0], [0.7, 0.7], [0, 1], [-0.7, 0.7], [-0.7, -0.7], [0.7, -0.7], [5, 1], [4.7, 4.7], [4, 5], [3.3, 4.7], [3.3, 3.3], [4.7, 3.3]]
 
-#print(data_to_pModule(data, 6))
+#======================================================================================
+#======================================================================================
+#======================================================================================
+# Example1
+
+#data = [[1, 0], [0.7, 0.7], [0, 1], [-0.7, 0.7], [-0.7, -0.7], [0.7, -0.7], [5, 1], [4.7, 4.7], [4, 5], [3.3, 4.7], [3.3, 3.3], [4.7, 3.3]]
+
+#print(homology_generators(data, 21))
+#print(data_to_PModule(data, 7))
+
+#======================================================================================
+# Exmaple 2
+
+import matplotlib.pyplot as plt
+
+# This function creates a loop of a given radius and center
+def create_loop(center, radius, num_points=100):
+    angles = np.linspace(0, 2 * np.pi, num_points)
+    x = center[0] + radius * np.cos(angles)
+    y = center[1] + radius * np.sin(angles)
+    return np.column_stack((x, y))
+
+
+small_loops = []
+for i in range(3):  
+    center = (np.random.uniform(-5, 5), np.random.uniform(-5, 5))
+    small_loops.append(create_loop(center, radius=1))
+
+
+big_loops = []
+for i in range(2):  
+    center = (np.random.uniform(-5, 5), np.random.uniform(-5, 5))
+    big_loops.append(create_loop(center, radius=3))
+
+data = np.vstack(small_loops + big_loops)
+
+# Optionally, plot the loops
+plt.figure(figsize=(8, 8))
+plt.scatter(data[:, 0], data[:, 1], s=5)
+plt.title("Point Data with Small and Big Loops")
+plt.xlabel("X-axis")
+plt.ylabel("Y-axis")
+plt.axis('equal')
+plt.grid()
+#plt.show() 
+
+# Finding the first radius `r` for which the set of 1-homology generators is non-empty
+r = 0
+hom = homology_generators(data, r)
+while homology_generators(data, r) == []:
+   r += 1
+   hom = homology_generators(data, r)
+   print(f'r = {r}: pending ..')
+   if hom != []:
+      print(f'The 1-homology generators for r = {r}:')
+      print(hom)
+      break
